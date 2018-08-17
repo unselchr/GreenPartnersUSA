0fbff19ee56fcc18d4641908002041b77c1cc854
unselchr <cdojrf@gmail.com>
2018-08-16 19:08:33 -0500

    server working

--- data/routes/routes.js
+++ data/routes/routes.js
@@ -1,4 +1,16 @@
-var routes={
-    
-}
-module.exports=routes;
+var express=require("express");
+var router=express.Router();
+//
+router.get("/aboutUs",function(req,res){
+    res.render("aboutUs",{});
+})
+router.post("/aboutUs",function(req,res){
+    //do something with the message
+    var data=req.body;
+})
+//catch all must be LAST
+router.get("*",function(req,res){
+    res.render("index",{});
+})
+//catch all must be LAST
+module.exports=router;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express=require("express");
 var app=express();
 var PORT=process.env.PORT || 3000;
+var exphbs=require("express-handlebars");
+var bodyParser=require("body-parser");
 //
 var routes=require("./data/routes/routes.js");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.engine("handlebars",exphbs({defaultLayout:"main"}));
+app.set("view engine","handlebars");
+app.use(express.static("public"));
+app.use(routes);
 //Host
 app.listen(PORT,function(){
     console.log("Server listening on port: "+PORT);
